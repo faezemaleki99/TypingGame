6645dac271e0f2e6dbf10a810e72d7fb18c81835
Faeze <faezemaleki012@gmail.com>
2024-12-26 12:51:37 +0330

    handling user input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from "faker";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TypingGame';
+  randomText: string = lorem.sentence()
+
+  onChangeInput(text: Event) {
+    console.log((text.target as HTMLInputElement).value)
+  }
 }
+
